d96f5461c30aec662938cb0d3d7d580016a308ce
DarrenPaulWright <darren@darrenpaulwright.com>
2020-03-11 16:26:39 -0700

    Adding custom eslintrc

--- index.js
+++ index.js
@@ -8,6 +8,7 @@
  */
 module.exports = {
 	benchSettings: require('./src/benchSettings.js'),
+	eslintrc: require('./src/eslintrc.js'),
 	formatBenchmark: require('./src/formatBenchmark.js'),
 	karmaBenchConfig: require('./src/karmaBenchConfig.js'),
 	karmaConfig: require('./src/karmaConfig.js'),
